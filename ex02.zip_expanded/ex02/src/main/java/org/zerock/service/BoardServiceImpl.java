4ef5e3dd69b50f3ad86500011ad9672f3813aee8
Seong_gu <sunggu507@naver.com>
2024-08-09 17:06:22 +0900

    feat : prev project open

---
+++ ex02.zip_expanded/ex02/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,59 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		log.info("register..............");
+		mapper.insertSelectKey(board);
+		
+	}
+
+	@Override
+	public List<BoardVO> getList(Criteria cri) {
+		log.info("getList...............");
+		return mapper.getListWithPaging(cri);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get...............");
+		return mapper.read(bno);
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		log.info("modify...............");
+		return mapper.update(board) == 1;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		log.info("remove...............");
+
+		return mapper.delete(bno) == 1;
+	}
+
+	@Override
+	public int getTotal(Criteria cri) {
+		
+		return mapper.getTotalCount(cri);
+	}
+
+}
---
+++ ex02.zip_expanded/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,76 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import lombok.extern.log4j.Log4j;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Test
+	public void testPaing() {
+		Criteria cri = new Criteria();
+		cri.setPageNum(1);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board));
+	}
+	
+//	@Test
+//	public void test() {
+//		mapper.getList().forEach(board -> log.info(board));
+//	}
+//	
+//	@Test
+//	public void testInsert() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로운글3");
+//		board.setContent("ggdgd333333");
+//		board.setWriter("newUSer3");
+//		
+//		mapper.insert(board);
+//		mapper.insertSelectKey(board);
+//	}
+
+//	@Test
+//	public void testSelect() {
+//		//존재하는 게시물 번호로 테스트
+//		BoardVO board = mapper.read(3L);
+//		log.info(board);
+//	}
+
+//	@Test
+//	public void testDelete() {
+//		log.info("Delete Count: " + mapper.delete(3L));
+//	}
+
+	
+//	@Test
+//	public void testUpdate() { 
+//		BoardVO board = new BoardVO();
+//		board.setBno(4L);
+//		board.setTitle("수정된 제목");
+//		board.setContent("수정된 내용");
+//		board.setWriter("user00");
+//		
+//		int count = mapper.update(board);
+//		log.info("Update count : " + count);
+//	}
+}
